533e4b6aff44002c10c5c8dfa64c7246f05fe038
scott.coleman <scott.coleman@bytecode.io>
2020-11-05 09:24:38 -0600

    Reduce logging

--- tap_pendo/streams.py
+++ tap_pendo/streams.py
@@ -247,10 +247,8 @@ class Stream():
             request_kwargs['json'] = kwargs.get('json')
 
         req = requests.Request(**request_kwargs).prepare()
-        LOGGER.info("%s %s %s %s %s %s", request_kwargs['method'],
-                    request_kwargs['url'], request_kwargs['headers'],
-                    request_kwargs['params'], request_kwargs.get('data', None),
-                    request_kwargs.get('json', None))
+        LOGGER.info("%s %s %s", request_kwargs['method'],
+                    request_kwargs['url'], request_kwargs['params'])
         resp = session.send(req)
 
         if 'Too Many Requests' in resp.reason:
