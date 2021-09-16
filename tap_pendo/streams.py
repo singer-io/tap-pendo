206affed7cb8f8e59aa5d4077a10d7800bad55f7
harshpatel4_crest <harsh.patel4@crestdatasys.com>
2021-09-16 14:44:44 +0530

    updated PK for track_events stream

--- tap_pendo/streams.py
+++ tap_pendo/streams.py
@@ -724,8 +724,11 @@ class PollEvents(Stream):
 class TrackEvents(EventsBase):
     replication_method = "INCREMENTAL"
     name = "track_events"
-    key_properties = ['visitor_id', 'account_id', 'server', 'remote_ip']
+    key_properties = ['track_type_id', 'visitor_id', 'account_id', 'server', 'remote_ip', 'user_agent']
 
+    def __init__(self, config):
+        super().__init__(config=config)
+        self.key_properties.append("day" if self.period == 'dayRange' else "hour")
 
     def get_body(self, key_id, period, first):
         return {
