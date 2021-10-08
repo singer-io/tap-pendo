1396df661b033706a74158eeb1e322375f1c9e1b
harshpatel4_crest <harsh.patel4@crestdatasys.com>
2021-10-08 16:40:03 +0530

    added unittest

---
+++ tests/unittests/test_track_events_primary_key.py
@@ -0,0 +1,36 @@
+import unittest
+from tap_pendo.streams import TrackEvents
+
+class TestTrackEventsPrimaryKey(unittest.TestCase):
+
+    def test_track_events_primary_key_with_hourRange(self):
+        '''
+            Verify that primary keys should have expected fields with 'hour' field when period is hourRange 
+        '''
+        config = {"period": "hourRange"} # set hourRange as a period
+
+        expected_primary_keys = ["track_type_id", "visitor_id", "account_id", "server", "remote_ip", "user_agent", "hour"]
+
+        event_stream1 = TrackEvents(config) # Initialize TrackEvents object which sets primary keys
+
+        # verify if the key properties macthes the expectation
+        self.assertEqual(event_stream1.key_properties, expected_primary_keys)
+
+        # Reset key properties for other test as it's class variable
+        TrackEvents.key_properties = ["track_type_id", "visitor_id", "account_id", "server", "remote_ip", "user_agent"]
+
+    def test_track_events_primary_key_with_dayRange(self):
+        '''
+            Verify that primary keys should have expected fields with 'day' field when period is dayRange 
+        '''
+        config = {"period": "dayRange"} # set dayRange as a period
+
+        expected_primary_keys = ["track_type_id", "visitor_id", "account_id", "server", "remote_ip", "user_agent", "day"]        
+
+        event_stream2 = TrackEvents(config) # Initialize TrackEvents object which sets primary keys
+
+        # verify if the key properties macthes the expectation
+        self.assertEqual(event_stream2.key_properties, expected_primary_keys)
+
+        # Reset key properties for other test as it's class variable
+        TrackEvents.key_properties = ["track_type_id", "visitor_id", "account_id", "server", "remote_ip", "user_agent"]
