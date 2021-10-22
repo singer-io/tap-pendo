4a5db32fe97a0f5df8e80e279b4053f81b3659fd
harshpatel4_crest <harsh.patel4@crestdatasys.com>
2021-10-22 08:51:08 +0530

    updated unittests, updated config.yml file

--- tests/unittests/test_track_events_primary_key.py
+++ tests/unittests/test_track_events_primary_key.py
@@ -7,6 +7,8 @@ class TestTrackEventsPrimaryKey(unittest.TestCase):
         '''
             Verify that primary keys should have expected fields with 'hour' field when period is hourRange 
         '''
+        # Reset key properties to default value
+        TrackEvents.key_properties = ["track_type_id", "visitor_id", "account_id", "server", "remote_ip", "user_agent"]
         config = {"period": "hourRange"} # set hourRange as a period
 
         expected_primary_keys = ["track_type_id", "visitor_id", "account_id", "server", "remote_ip", "user_agent", "hour"]
@@ -16,13 +18,12 @@ class TestTrackEventsPrimaryKey(unittest.TestCase):
         # verify if the key properties macthes the expectation
         self.assertEqual(event_stream1.key_properties, expected_primary_keys)
 
-        # Reset key properties for other test as it's class variable
-        TrackEvents.key_properties = ["track_type_id", "visitor_id", "account_id", "server", "remote_ip", "user_agent"]
-
     def test_track_events_primary_key_with_dayRange(self):
         '''
             Verify that primary keys should have expected fields with 'day' field when period is dayRange 
         '''
+        # Reset key properties to default value
+        TrackEvents.key_properties = ["track_type_id", "visitor_id", "account_id", "server", "remote_ip", "user_agent"]
         config = {"period": "dayRange"} # set dayRange as a period
 
         expected_primary_keys = ["track_type_id", "visitor_id", "account_id", "server", "remote_ip", "user_agent", "day"]        
@@ -31,6 +32,3 @@ class TestTrackEventsPrimaryKey(unittest.TestCase):
 
         # verify if the key properties macthes the expectation
         self.assertEqual(event_stream2.key_properties, expected_primary_keys)
-
-        # Reset key properties for other test as it's class variable
-        TrackEvents.key_properties = ["track_type_id", "visitor_id", "account_id", "server", "remote_ip", "user_agent"]
