64ff79d1a54fb2e0d5a7659426884446475a3511
savan-chovatiya <savan.chovatiya@crestdatasys.com>
2022-01-20 19:48:36 +0530

    Added unit test and fix integration test

--- tests/tap_tester/test_child_stream_start_date.py
+++ tests/tap_tester/test_child_stream_start_date.py
@@ -9,7 +9,7 @@ class PendoChildStreamStartDateTest(TestPendoBase):
 
     def test_run(self):
 
-        streams_to_test = {"guides", "guide_events"}
+        streams_to_test = {"features", "feature_events"}
 
         conn_id = connections.ensure_connection(self)
 
@@ -29,34 +29,34 @@ class PendoChildStreamStartDateTest(TestPendoBase):
             self.assertGreater(record_count_by_stream.get(stream, -1), 0,
                 msg="failed to replicate any data for stream : {}".format(stream))
 
-        # collect "guide" and "guide_events" data
-        guides = synced_records.get("guides")
-        guide_events = synced_records.get("guide_events")
+        # collect "feature" and "feature_events" data
+        features = synced_records.get("features")
+        feature_events = synced_records.get("feature_events")
 
-        # find the first guide's id
-        first_guide_id = guides.get("messages")[0].get("data").get("id")
+        # find the first feature's id
+        first_feature_id = features.get("messages")[0].get("data").get("id")
 
-        first_guide_ids_events = []
-        rest_guide_events = []
+        first_feature_ids_events = []
+        rest_feature_events = []
 
-        # seperate guide events based on guide id
-        for guide_event in guide_events.get("messages"):
-            if guide_event.get("data").get("guide_id") == first_guide_id:
-                first_guide_ids_events.append(guide_event.get("data"))
+        # seperate feature events based on feature id
+        for feature_event in feature_events.get("messages"):
+            if feature_event.get("data").get("feature_id") == first_feature_id:
+                first_feature_ids_events.append(feature_event.get("data"))
             else:
-                rest_guide_events.append(guide_event.get("data"))
+                rest_feature_events.append(feature_event.get("data"))
 
-        replication_key_for_guide_events = next(iter(self.expected_replication_keys().get("guide_events")))
+        replication_key_for_feature_events = next(iter(self.expected_replication_keys().get("feature_events")))
 
-        # find the maximun bookmark date for first guide's events
-        sorted_first_guide_ids_events = sorted(first_guide_ids_events, key=lambda i: i[replication_key_for_guide_events], reverse=True)
-        max_bookmark = sorted_first_guide_ids_events[0].get(replication_key_for_guide_events)
+        # find the maximun bookmark date for first feature's events
+        sorted_first_feature_ids_events = sorted(first_feature_ids_events, key=lambda i: i[replication_key_for_feature_events], reverse=True)
+        max_bookmark = sorted_first_feature_ids_events[0].get(replication_key_for_feature_events)
 
-        # used for verifying if we synced guide events before
-        # than the maximum bookmark of first guide's events
+        # used for verifying if we synced feature events before
+        # than the maximum bookmark of first feature's events
         synced_older_data = False
-        for rest_guide_event in rest_guide_events:
-            event_time = datetime.strptime(rest_guide_event.get(replication_key_for_guide_events), "%Y-%m-%dT%H:%M:%S.%fZ")
+        for rest_feature_event in rest_feature_events:
+            event_time = datetime.strptime(rest_feature_event.get(replication_key_for_feature_events), "%Y-%m-%dT%H:%M:%S.%fZ")
             max_bookmark_time = datetime.strptime(max_bookmark, "%Y-%m-%dT%H:%M:%S.%fZ")
             if event_time < max_bookmark_time:
                 synced_older_data = True
