292836c104d2fe7da2de23f1e59c2d36c7b34e8b
Dylan <28106103+dsprayberry@users.noreply.github.com>
2022-09-08 11:09:28 -0400

    Version Bump and Changelog Update (#113)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="tap-pendo",
-    version="0.4.2",
+    version="0.4.3",
     description="Singer.io tap for extracting data",
     author="Stitch",
     url="https://github.com/singer-io/tap-pendo",
