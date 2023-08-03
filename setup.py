4b10c85c5541b9faf8bd12c3c6fd39fe04e48759
nitingaikwad1 <102946865+nitingaikwad1@users.noreply.github.com>
2023-08-03 14:34:29 +0530

    bump version 1.0.1 (#128)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="tap-pendo",
-    version="1.0.0",
+    version="1.0.1",
     description="Singer.io tap for extracting data",
     author="Stitch",
     url="https://github.com/singer-io/tap-pendo",
