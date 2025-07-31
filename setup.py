3f59f3c6e9e20afc4c68cbe38cbb6b5370d9bfe2
Prijen Khokhani <88327452+prijendev@users.noreply.github.com>
2025-07-31 18:24:34 +0530

    Bump tap version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="tap-pendo",
-    version="1.1.1",
+    version="1.2.0",
     description="Singer.io tap for extracting data",
     author="Stitch",
     url="https://github.com/singer-io/tap-pendo",
